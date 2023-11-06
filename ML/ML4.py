54970112fb2e737d78e9b7fdac51e9b4477e2ad1
Sahil030303 <91740934+Sahil030303@users.noreply.github.com>
2023-11-06 23:07:01 +0530

    Add files via upload

---
+++ ML/ML4.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import pyplot
+
+
+# In[2]:
+
+
+current_x = 2
+rate = 0.01 # Learning rate
+precision = 0.000001  # This tells us when to stop the algorithm
+delta_x = 1
+max_iterations = 10000 # Maximum number of iterations
+iteration_counter = 0
+
+
+# In[3]:
+
+
+# dy/dx of eqn = 2*(x+3)
+def slope(x):
+    return 2*(x+3)
+
+def value_y(x):
+    return (x+3)**2
+y = []
+x = []
+y.append(value_y(current_x))
+x.append(current_x)
+
+
+# In[4]:
+
+
+# Generate x values
+xi = np.linspace(-8, 2, 100)  # Adjust the range as needed
+
+# Calculate y values for the curve
+yi = (xi + 3)**2
+
+# Create the plot
+plt.plot(xi, yi)
+
+# Add labels and a title
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Curve: y = (x + 3)^2")
+
+# Display the plot
+plt.grid(True)  # Add grid lines if desired
+plt.show()
+
+
+# In[5]:
+
+
+while delta_x > precision and iteration_counter < max_iterations:
+    previous_x = current_x
+    current_x = previous_x - rate * slope(previous_x)
+    y.append(value_y(current_x))
+    x.append(current_x)
+    delta_x = abs(previous_x - current_x)
+    print(f"Iteration {iteration_counter+1}")
+    iteration_counter += 1
+    print(f"X = {current_x}")
+
+print(f"Local Minima occurs at: {current_x}")
+
+
+# In[13]:
+
+
+pyplot.plot(x,y,'.-')
+plt.xlabel('x-values')
+plt.ylabel('y-values')
+plt.title('y=(x+3)^2')
+plt.show()
+
