0feda55d383326502d44ff8a25bac73107716e8a
Sahil030303 <91740934+Sahil030303@users.noreply.github.com>
2023-11-07 11:29:06 +0530

    Add files via upload

--- DAA/DAA_6.py
+++ DAA/DAA_6.py
@@ -2,14 +2,18 @@
 def partition(array, low, high):
     pivot = array[low]
     i = low + 1
-
-    for j in range(low + 1, high + 1):
-        if array[j] <= pivot:
+    j = high
+    while True:
+        while i <= j and array[i] <= pivot:
+            i += 1
+        while i <= j and array[j] >= pivot:
+            j -= 1
+        if i <= j:
             array[i], array[j] = array[j], array[i]
-            i = i + 1
-
-    array[low], array[i - 1] = array[i - 1], array[low]
-    return i - 1
+        else:
+            break
+    array[low], array[j] = array[j], array[low]
+    return j
 
 def quickSort(array, low, high):
     if low < high:
@@ -17,7 +21,7 @@ def quickSort(array, low, high):
         quickSort(array, low, pi - 1)
         quickSort(array, pi + 1, high)
 
-data = [1, 7, 4, 10, 9, -2]
+data = [1, 7, 4, 10, 9, -2, 0, -2, 4, 11]
 print("Unsorted Array")
 print(data)
 
